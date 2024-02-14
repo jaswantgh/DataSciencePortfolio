5a8308a532183e0f497ddb190f5c0d1e7aff52c5
Jaswant Pendem <jaswant.pendem@gmail.com>
2024-02-14 21:46:38 +0800

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -369,6 +369,13 @@ const contactInfo = {
   email_address: "saadpasta70@gmail.com"
 };
 
+// Twitter Section
+
+const twitterDetails = {
+  userName: "twitter", //Replace "twitter" with your twitter username without @
+  display: true // Set true to display this section, defaults to false
+};
+
 const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
 
 export {
@@ -387,5 +394,6 @@ export {
   talkSection,
   podcastSection,
   contactInfo,
+  twitterDetails,
   isHireable
 };
