e099fa921db36d66bb52fd4e855f8c8f601d9cbb
jaswantgh <138118307+jaswantgh@users.noreply.github.com>
2024-02-14 21:23:10 +0800

    Update portfolio.js
    Blog Change

--- src/portfolio.js
+++ src/portfolio.js
@@ -324,12 +324,6 @@ const blogSection = {
       title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
       description:
         "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
-    },
-    {
-      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-      title: "Why REACT is The Best?",
-      description:
-        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
     }
   ],
   display: true // Set false to hide this section, defaults to true
