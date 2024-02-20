dcb60dd463147460f1ce29a4e1a37fbbad0c91c9
jaswantgh <pendem.jaswant@gmail.com>
2024-02-20 21:27:58 +0800

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -201,30 +201,6 @@ const workExperiences = {
         }
       ],
       
-    }
-    
-  ]
-};
-
-
-// Other experience section
-
-const otherExperiences = {
-  display: true, //Set it to true to show otherExperiences Section
-  experience1: [
-    {
-      role: "Engineering Intern",
-      company: "CalcTree",
-      companylogo: require("./assets/images/calctreeLogo.jpeg"),
-      date: "Nov 2023 – Mar 2024",
-      desc: " Progressively working on 450 Calculator templates wiht a team of 11. Creating calculation templates and resources that are utilized by engineers worldwide, demonstrating my ability to produce valuable content. Collaborated with the tech team to test early versions of the CalcTree platform and provide feedback on upcoming designs",
-      footerLink: [
-        {
-          name: "Letter of Acceptance",
-          url: "https://drive.google.com/file/d/1yF0P_8P3BZ1slZ5oTG6dIympTwkh9xDM/view?usp=sharing"
-        }
-      ],
-      
     },
     {
       role: "Electrical Engineering Research Intern",
@@ -293,6 +269,31 @@ const otherExperiences = {
       date: "Feb 2023 – Present",
       desc: "Lorem olore magna aliqua.",
     }
+    
+  ]
+};
+
+
+// Other experience section
+
+const otherExperiences = {
+  display: true, //Set it to true to show otherExperiences Section
+  experience1: [
+    {
+      role: "Engineering Intern",
+      company: "CalcTree",
+      companylogo: require("./assets/images/calctreeLogo.jpeg"),
+      date: "Nov 2023 – Mar 2024",
+      desc: " Progressively working on 450 Calculator templates wiht a team of 11. Creating calculation templates and resources that are utilized by engineers worldwide, demonstrating my ability to produce valuable content. Collaborated with the tech team to test early versions of the CalcTree platform and provide feedback on upcoming designs",
+      footerLink: [
+        {
+          name: "Letter of Acceptance",
+          url: "https://drive.google.com/file/d/1yF0P_8P3BZ1slZ5oTG6dIympTwkh9xDM/view?usp=sharing"
+        }
+      ],
+      
+    },
+    
     
   ]
 };
